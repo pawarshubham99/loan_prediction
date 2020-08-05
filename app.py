70bb085d0174a2d691b0f5ff7467cc9c3bf86c1b
Shubham Pawar <61535043+pawarshubham99@users.noreply.github.com>
2020-08-05 17:14:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask import Flask, request, jsonify, render_template
 
 import pandas as pd
 import joblib
-#model = joblib.load('model.joblib')
+
 #model = joblib.load('model.pkl')
 app = Flask(__name__)
 #model = pickle.load(open('model.pkl', 'rb'))
-
+model = joblib.load('model.joblib')
 df = None
 @app.route("/")
 @app.route('/upload', methods=['GET', 'POST'])
